bdf38e689edbc7ad4cc78127852f53a6e6c6b23b
amaljofy10 <amaljofy10@gmail.com>
2021-01-12 11:17:49 +0530

    Lamp core removed

--- app/src/main/java/digital/lamp/lamp_kotlin_test/MainActivity.kt
+++ app/src/main/java/digital/lamp/lamp_kotlin_test/MainActivity.kt
@@ -3,6 +3,8 @@ package digital.lamp.lamp_kotlin_test
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import digital.lamp.lamp_core.models.SensorEvent
+import digital.lamp.sensor_core.Accelerometer
+import digital.lamp.sensor_core.Lamp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
