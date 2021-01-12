1d36b13db2f0b4fc8351231138f8c2a628bbc154
amaljofy10 <amaljofy10@gmail.com>
2021-01-12 10:46:05 +0530

    Initial commit

---
+++ app/src/main/java/digital/lamp/lamp_kotlin_test/MainActivity.kt
@@ -0,0 +1,14 @@
+package digital.lamp.lamp_kotlin_test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import digital.lamp.lamp_core.models.SensorEvent
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        SensorEvent
+    }
+}
